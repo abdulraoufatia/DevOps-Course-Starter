80f2a8698b11e9be01843469561fb606942bfd26
abdulraoufatia <74622061+abdulraoufatia@users.noreply.github.com>
2021-10-03 22:51:55 +0100

    Update app.py

--- todo_app/app.py
+++ todo_app/app.py
@@ -8,4 +8,20 @@ app.config.from_object(Config())
 
 @app.route('/')
 def index():
-    return 'Hello World!'
+    return """
+    <html>
+<head>
+<style>
+h1 {text-align: center;}
+p {text-align: center;}
+</style>
+</head>
+<body>
+
+<h1>My todo list</h1>
+<p>What do you wish to complete mi amigo?</p>
+
+</body>
+</html>
+    
+    """

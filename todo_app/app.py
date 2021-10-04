643516c60ec4ba96fa0b724194c116c767042495
abdulraoufatia <74622061+abdulraoufatia@users.noreply.github.com>
2021-10-04 16:54:08 +0100

    Update app.py

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, request
+from flask import Flask, render_template
 
 from todo_app.flask_config import Config
 
@@ -6,12 +6,9 @@ app = Flask(__name__)
 app.config.from_object(Config())
 
 from todo_app.data.session_items import get_items
-from todo_app.data.session_items import add_item(title) 
 
-@app.route('/',  methods=["POST", "GET"])
+@app.route('/')
 def index():
-    get_items = ['item1','item2','item3']
-    add_item(title=[1,2,3,4])
-    if request.method == 'POST':
-        return do_the_login()
     return render_template('index.html', get_items=get_items)
+    session
+    

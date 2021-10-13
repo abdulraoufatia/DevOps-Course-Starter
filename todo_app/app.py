42ee903d8fa6c7e852d2157332c79061b8c33026
abdulraoufatia <74622061+abdulraoufatia@users.noreply.github.com>
2021-10-13 12:41:21 +0100

    module_1

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,14 +1,17 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect, url_for
+from flask.helpers import url_for
 
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config())
 
-from todo_app.data.session_items import get_items
 
-@app.route('/')
+from todo_app.data.session_items import add_item, get_items
+
+
+@app.route('/', methods = ['POST', 'GET'])
 def index():
+    request.form.get('title')
     return render_template('index.html', items = get_items())
-    session
-    
+

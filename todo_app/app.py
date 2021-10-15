42fe4f00dfdb7f2026528d5cf8a0a11eb61e6487
abdulraoufatia <74622061+abdulraoufatia@users.noreply.github.com>
2021-10-15 13:59:42 +0100

    get_items function added

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,14 +1,25 @@
 from flask import Flask, render_template, request, redirect, url_for
-from flask.helpers import url_for
-
+from todo_app.data.session_items import get_items
 from todo_app.flask_config import Config
 
 app = Flask(__name__)
 app.config.from_object(Config())
 
-from todo_app.data.session_items import get_items
-
 @app.route('/', methods = ['POST', 'GET'])
 def index():
-    request.form.get('title')
-    return render_template('index.html', items = get_items())
+    items = get_items()
+    return render_template('index.html', items = items, get_items = get_items)
+
+@app.route('/newitem', methods = ['POST'])
+def add_item():
+    if request.method == 'POST':
+      item = request.form['nm']
+      return redirect(url_for('success',add_item = add_item))
+    else:
+      user = request.args.get('nm')
+      return redirect(url_for('success',add_item = add_item))
+    
+    return ()
+
+if __name__ == '__main__':
+    app.run()

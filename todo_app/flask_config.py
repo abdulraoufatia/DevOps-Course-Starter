f0e76873bac0914b4e3ee5750034a65c85183177
aatia1 <16aatia@gmail.com>
2022-02-07 03:01:43 +0000

    trying to fix, still broken

--- todo_app/flask_config.py
+++ todo_app/flask_config.py
@@ -4,6 +4,9 @@ import os
 class Config:
     def __init__(self):
         """Base configuration variables."""
+        self.API_KEY = os.environ.get("API_KEY")
+        self.API_TOKEN = os.environ.get("API_TOKEN")
+        self.BOARD = os.environ.get("BOARD")
         self.SECRET_KEY = os.environ.get("SECRET_KEY")
         if not self.SECRET_KEY:
             raise ValueError(

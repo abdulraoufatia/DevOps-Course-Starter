d8f6a9d654358a87354ac8c8c7f015b1d1af9325
Abdulraouf Atia <abdulraoufatia@outlook.com>
2022-10-19 14:33:35 +0100

    Module_10: Part 2 - Attempt to fix Failed Action

--- todo_app/test/test_db.py
+++ todo_app/test/test_db.py
@@ -8,7 +8,7 @@ def client():
     file_path = find_dotenv('.env.test')
     load_dotenv(file_path, override=True)
     
-    with mongomock.patch(servers=(('fakemongo.com', 27017),)):
+    with mongomock.patch(servers=(('mongodb://fakemongo.com', 27017),)):
         test_app = app.create_app()
         with test_app.test_client() as client:
             yield client

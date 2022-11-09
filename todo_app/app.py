63e4a997691af9db1226dcc870337bd973447315
Abdulraouf Atia <abdulraoufatia@outlook.com>
2022-11-09 13:37:59 +0000

    Saving attempt in tests

--- todo_app/app.py
+++ todo_app/app.py
@@ -1,12 +1,6 @@
 from os import name
 from flask import Flask, render_template, request, redirect, url_for
-from todo_app.data.mongo_items import (
-    get_all_cards,
-    create_mongo_item,
-    complete_mongo_item,
-    in_progress_mongo_item,
-    delete_item,
-)
+from todo_app.data.mongo_items import MongoClient
 from todo_app.flask_config import Config
 from todo_app.data.todoitem import ToDoItem
 from todo_app.data.viewmodel import ViewModel
@@ -16,10 +10,12 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object(Config())
 
+    mongo_client = MongoClient()
+
     # Part 1, Step 1 - Module_3: Making a ViewModel
     @app.route("/", methods=["GET"])
     def index():
-        todo_items = get_all_cards()
+        todo_items = mongo_client.get_all_cards()
         # Part 1, Step 1 - Module_3: Making a ViewModel
         view_model = ViewModel(todo_items)
         return render_template("index.html", view_model=view_model)
@@ -27,20 +23,20 @@ def create_app():
     @app.route("/newitem", methods=["POST"])
     def new_item():
         item = request.form["todo"]
-        create_mongo_item(item)
+        mongo_client.create_mongo_item(item)
         return redirect(url_for("index"))
 
     @app.route("/in_progress", methods=["POST"])
     def mark_item_in_progress():
         id = request.form.get("inprogress")
-        in_progress_mongo_item(id)
+        mongo_client.in_progress_mongo_item(id)
         return redirect(url_for("index"))
 
     @app.route("/complete_item", methods=["POST"])
     def complete_item():
         if request.method == "POST":
             id = request.form["id"]
-            complete_mongo_item(id)
+            mongo_client.complete_mongo_item(id)
             return redirect(url_for("index"))
         else:
             return render_template()
--- todo_app/test/test_db.py
+++ todo_app/test/test_db.py
@@ -8,15 +8,19 @@ def client():
     file_path = find_dotenv('.env.test')
     load_dotenv(file_path, override=True)
     
-    with mongomock.patch(servers=(('mongodb+srv://fakemongo.com', 27017),)):
+    with mongomock.patch(servers=(('fakemongo.com', 27017),)):
         test_app = app.create_app()
         with test_app.test_client() as client:
             yield client
 
 
 def test_index_page(client):
+    # Arrange
+    # MISSING!
     
+    # Act
     response = client.get("/")
 
+    # Assert 
     assert response.status_code == 200
     assert "Test card" in response.data.decode()

a8ae4ed21279b2c29fac3fbe0ef0a8a43c3c62e7
Abdulraouf Atia <abdulraoufatia@outlook.com>
2022-11-16 13:55:45 +0000

    Module 1: Part 1

--- todo_app/app.py
+++ todo_app/app.py
@@ -4,16 +4,24 @@ from todo_app.data.mongo_items import MongoClient
 from todo_app.flask_config import Config
 from todo_app.data.todoitem import ToDoItem
 from todo_app.data.viewmodel import ViewModel
+from flask_login import LoginManager, login_required, login_user
+import os, requests
+from todo_app.data.user import User
+
+client_id='CLIENTID'
+client_secret='CLIENTSECRET'
 
 
 def create_app():
     app = Flask(__name__)
     app.config.from_object(Config())
+    login_manager = LoginManager()
 
     mongo_client = MongoClient()
 
     # Part 1, Step 1 - Module_3: Making a ViewModel
     @app.route("/", methods=["GET"])
+    @login_required
     def index():
         todo_items = mongo_client.get_all_cards()
         # Part 1, Step 1 - Module_3: Making a ViewModel
@@ -21,18 +29,21 @@ def create_app():
         return render_template("index.html", view_model=view_model)
 
     @app.route("/newitem", methods=["POST"])
+    @login_required
     def new_item():
         item = request.form["todo"]
         mongo_client.create_mongo_item(item)
         return redirect(url_for("index"))
 
     @app.route("/in_progress", methods=["POST"])
+    @login_required
     def mark_item_in_progress():
         id = request.form.get("inprogress")
         mongo_client.in_progress_mongo_item(id)
         return redirect(url_for("index"))
 
     @app.route("/complete_item", methods=["POST"])
+    @login_required
     def complete_item():
         if request.method == "POST":
             id = request.form["id"]
@@ -42,10 +53,53 @@ def create_app():
             return render_template()
 
     @app.route("/delete_card", methods=["POST"])
+    @login_required
     def deleting_card_function():
         delete_item = request.form["delete_card"]
         delete_item(delete_item)
         return redirect(url_for("index"))
+
+    @login_manager.unauthorized_handler
+    def unauthenticated():
+        return redirect ('https://github.com/login/oauth/authorize?client_id='+ os.getenv(client_id))
+    
+    @login_manager.user_loader
+    def load_user(user_id):
+        print(user_id)
+        pass # We will return to this later
+
+    login_manager.init_app(app)
+
+    @app.route('/login/callback')
+    def login():
+        code= request.args.get('code')
+        access_token_url = 'https://github.com/login/oauth/access_token'
+        payload = {
+            'client_id': os.getenv(client_id),
+            'client_secret': os.getenv(client_secret),
+            'code': code,
+        } 
+        r = requests.post(access_token_url, json=payload, headers={'Accept': 'application/json'})
+        access_token = r.json().get('access_token')
+        print(access_token)
+        reqUrl = "https://api.github.com/user"
+
+        headersList = {
+        "Accept": "*/*",
+        "Authorization": f"Bearer {access_token}" 
+        }
+
+        response = requests.get(reqUrl, headers=headersList)
+
+        response_json = response.json()
+
+        print(response_json)
+
+        user = User(response_json['id'])
+
+        login_user(user)
+        return redirect('/')
+
 
     return app
 

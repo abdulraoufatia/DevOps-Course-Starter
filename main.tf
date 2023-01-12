7f043751bf1a89e90f89b3babac79a1e11d1badc
Abdulraouf Atia <abdulraoufatia@outlook.com>
2023-01-12 13:44:13 +0000

    Module_12: Part 1 - Step 1 - 4

---
+++ main.tf
@@ -0,0 +1,95 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = ">= 3.8"
+    }
+  }
+}
+provider "azurerm" {
+  features {}
+}
+data "azurerm_resource_group" "main" {
+  name = "KPMG21_AbdulraoufAtia_ProjectExercise"
+}
+
+resource "azurerm_service_plan" "main" {
+  name                = "terraformed-asp"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+  os_type             = "Linux"
+  sku_name            = "B1"
+}
+resource "azurerm_linux_web_app" "main" {
+  name                = "ARA-TODO-APP"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main.name
+  service_plan_id     = azurerm_service_plan.main.id
+  site_config {
+    application_stack {
+      docker_image     = "appsvcsample/python-helloworld"
+      docker_image_tag = "latest"
+    }
+  }
+  app_settings = {
+    "DOCKER_REGISTRY_SERVER_URL" = "https://index.docker.io"
+  }
+}
+
+resource "azurerm_cosmosdb_account" "db" {
+  name                = "ara-todo-appm12"
+  location            = data.azurerm_resource_group.main.location
+  resource_group_name = data.azurerm_resource_group.main
+  offer_type          = "Standard"
+  kind                = "MongoDB"
+
+  enable_automatic_failover = true
+
+  capabilities {
+    name = "EnableAggregationPipeline"
+  }
+
+  capabilities {
+    name = "mongoEnableDocLevelTTL"
+  }
+
+  capabilities {
+    name = "MongoDBv3.4"
+  }
+
+  capabilities {
+    name = "EnableMongo"
+  }
+
+  capabilities { 
+    name = "EnableServerless" 
+  }
+
+  consistency_policy {
+    consistency_level       = "BoundedStaleness"
+    max_interval_in_seconds = 300
+    max_staleness_prefix    = 100000
+  }
+
+  geo_location {
+    location          = "uksouth"
+    failover_priority = 1
+  }
+
+  geo_location {
+    location          = "southuk"
+    failover_priority = 0
+  }
+}
+
+resource "azurerm_cosmosdb_mongo_database" "example" {
+  name                = "md12-cosmos-db"
+  resource_group_name = data.azurerm_cosmosdb_account.db.resource_group_name
+  account_name        = data.azurerm_cosmosdb_account.db.name
+  throughput          = 400
+}
+
+output "cosmosdb_connectionstrings" {
+   value = "AccountEndpoint=${azurerm_cosmosdb_account.db.endpoint};AccountKey=${azurerm_cosmosdb_account.db.primary_key};"
+   sensitive   = true
+}
